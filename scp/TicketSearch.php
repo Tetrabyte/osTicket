b1d0a73faaa06f11f49fa35a9756814c4f930d7e
Antt1995 <admin@antts.xyz>
2022-03-07 12:15:03 +0000

    CSS

--- scp/TicketSearch.php
+++ scp/TicketSearch.php
@@ -103,6 +103,9 @@ $ost->setPageTitle('Ticket Search Tool');
     <meta charset="utf-8">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 	  <style>
+table {
+	 border-collapse: collapse !important;
+}
 *, ::after, ::before {
   box-sizing: content-box;
 }
