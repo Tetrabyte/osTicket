01759845a6e1136a6568715e00398d3ff5145eec
Antt1995 <antt1995@antts.uk>
2023-07-04 23:25:43 +0100

    Revert "RM Auth Backend"
    This reverts commit c2d2a811f8ba99971acdd629578d9d9c92b34837.

--- include/plugins/stafflogin/authentication.php
+++ include/plugins/stafflogin/authentication.php
@@ -11,9 +11,9 @@ class AnttStaffLogin extends Plugin {
         $staffAccess = $config->get('HIDE_LOCAL_STAFF_LOGIN');
         if ($staffAccess) {
           require_once('StaffLogin.php');
-          #StaffAuthenticationBackend::register(
-          AnttStaffLoginBackend($this->getConfig()); #);
-        #}
+          StaffAuthenticationBackend::register(
+                new AnttStaffLoginBackend($this->getConfig()));
+        }
     }
 }
 
