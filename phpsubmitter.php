87d2e093b82877248ec8bf05393546027ffeb736
Antt1995 <admin@antts.xyz>
2023-07-10 10:06:07 +0100

    Add Prints to API

--- phpsubmitter.php
+++ phpsubmitter.php
@@ -1,37 +1,52 @@
 #!/usr/bin/php -q
 <?php
-include_once('include/api-config.php');
-$config = array(
-  'url'=> 'https://tickets.remoteit.co.uk/api/http.php/tickets.json',
-);
-$name = $_GET['name'];
-$email = $_GET['email'];
-$phone = $_GET['phone'];
-$subject = $_GET['subject'];
-$message = $_GET['message'];
-$notes = $_GET['note'];
-$data = array(
-    'name'      =>    $name,
-    'email'       =>    $email,
-    'subject'    =>    $subject,
-    'message' =>    $message,
-    'notes'       =>    $notes,
-    'phone'      =>    $phone,
-    'ip'             =>    '80.244.186.132',
-	'topicId'   =>      '10',
-);
+
+include_once 'include/api-config.php';
+
+$config = [
+    'url' => 'https://tickets.remoteit.co.uk/api/http.php/tickets.json',
+];
+
+$name = $_GET['name'] ?? null;
+$email = $_GET['email'] ?? null;
+$phone = $_GET['phone'] ?? null;
+$subject = $_GET['subject'] ?? null;
+$message = $_GET['message'] ?? null;
+$notes = $_GET['note'] ?? null;
+
+$data = [
+    'name' => $name,
+    'email' => $email,
+    'subject' => $subject,
+    'message' => $message,
+    'notes' => $notes,
+    'phone' => $phone,
+    'ip' => '80.244.186.132',
+    'topicId' => '10',
+];
 
 set_time_limit(30);
-$options = array(
-  'http' => array(
-    'header'  => "X-API-Key: ".$API_KEY,
-    'method' => 'POST',
-    'content' => json_encode($data)
-   )
-);
-$context  = stream_context_create($options);
-$result = file_get_contents($config['url'], false, $context);
-if ($result === FALSE) { die("FAILED"); }
-echo($result);
 
-?>
+$options = [
+    'http' => [
+        'header' => "X-API-Key: {$API_KEY}",
+        'method' => 'POST',
+        'content' => json_encode($data),
+    ],
+];
+
+$context = stream_context_create($options);
+$result = file_get_contents($config['url'], false, $context);
+
+if ($result === false) {
+    die('FAILED');
+}
+
+
+print_r("<br><br>");
+print_r($data);
+print_r("<br><br>");
+print_r($options);
+print_r("<br><br>");
+
+echo $result;
